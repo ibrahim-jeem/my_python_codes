552b32598d5d5a990a7d112e916e9d5d39b03641
Mohammad Ibrahim <ibrahim_jeem@rocketmail.com>
2020-10-05 08:14:40 +0600

    Create Django_server.py

---
+++ Django_server.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+import os
+import requests
+
+list_of_files = os.listdir('/data/feedback')
+for file in list_of_files:
+        fp = open('/data/feedback/'+file)
+        data = fp.read()
+        data = data.split('\n')
+        dic = {"title":data[0], "name":data[1], "date":data[2], "feedback":data[3]}
+        response = requests.post('http://104.197.6.205/feedback/', json=dic)
+        print(response.status_code)
